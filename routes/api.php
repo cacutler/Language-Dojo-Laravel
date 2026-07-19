971ced6c2f24d02481ef40f7af0e65ad70d9475d
cacutler <calexcutler@gmail.com>
2026-07-19 15:09:19 -0600

    Updated code styling

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
@@ -14,15 +13,11 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::apiResource('languages', LanguageController::class);
     Route::apiResource('languages.courses', CourseController::class)->only(['index', 'store']);
-
     Route::apiResource('courses', CourseController::class)->except(['index', 'store']);
     Route::apiResource('courses.grammar-rules', GrammarRuleController::class)->only(['index', 'store']);
-
     Route::apiResource('grammar-rules', GrammarRuleController::class)->except(['index', 'store']);
     Route::apiResource('grammar-rules.system-examples', SystemExampleController::class)->only(['index', 'store']);
-
     Route::apiResource('system-examples', SystemExampleController::class)->except(['index', 'store']);
-
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('user-examples', UserExampleController::class);
         Route::apiResource('progress-tracking', ProgressTrackingController::class);
