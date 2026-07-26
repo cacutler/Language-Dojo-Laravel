e5c47960cec140172f35645bf66412b2aab636a9
cacutler <calexcutler@gmail.com>
2026-07-26 15:05:40 -0600

    added content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,24 @@
 @section('title', 'Language Dojo')
 @section('content')
 <h1>Language Dojo</h1>
+<p id="intro">Welcome to Language Dojo.</p>
+<div id="main-page">
+    <section>
+        <p>Features</p>
+        <ul>
+            <li>Ability to track progress for different language grammar rules.</li>
+            <li>Ability to add your own examples for different language grammar rules.</li>
+        </ul>
+    </section>
+    <section>
+        <p>Languages Covered</p>
+        <ul>
+            <li>English</li>
+            <li>Korean</li>
+            <li>Japanese</li>
+            <li>Spanish</li>
+            <li>And More!</li>
+        </ul>
+    </section>
+</div>
 @endsection
